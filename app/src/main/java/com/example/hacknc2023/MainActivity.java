c34662d14ab45b541a45f9de9abac20b38895cbb
shengiv <66622728+shengiv@users.noreply.github.com>
2023-10-29 06:38:43 +0800

    Add add activity form UI

--- app/src/main/java/com/example/hacknc2023/MainActivity.java
+++ app/src/main/java/com/example/hacknc2023/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.hacknc2023;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void beginForm(View view) {
+        AddActivity dialogFragment = new AddActivity();
+        dialogFragment.show(getSupportFragmentManager(), "UserFormDialog");
     }
 }
