36053d7fa5357cf23c1ac760319a2ec0441ea31a
thomaslb <lagaba@live.unc.edu>
2023-10-29 03:05:21 -0400

    Use API to pick location of meetup

--- app/src/main/java/com/example/hacknc2023/MainActivity.java
+++ app/src/main/java/com/example/hacknc2023/MainActivity.java
@@ -20,46 +20,6 @@ public class MainActivity extends AppCompatActivity {
         createNewUser();
         Log.i("firebaseUser", HandleDatabase.getInstance().getUser(currUserId));
     }
-
-
-    public String searchLocation(String keyword) {
-
-        Thread thread = new Thread(new Runnable() {
-
-            public void run() {
-                OkHttpClient client = new OkHttpClient();
-                String apiKey = "AIzaSyAjpMcfOWq14zsy-2CxTEnLWhxOPCvbq2M";
-                String location = "35.905463, -79.048869";
-                int radius = 5000;
-                String url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + keyword + "&location=" + location + "&radius=" + radius + "&key=" + apiKey;
-
-                Request request = new Request.Builder()
-                        .url(url)
-                        .build();
-
-                try {
-                    Response response = client.newCall(request).execute();
-                    String jsonData = response.body().string();
-
-                    JSONObject jsonObject = new JSONObject(jsonData);
-                    JSONArray results = jsonObject.getJSONArray("results");
-                    Random random = new Random();
-                    int randomIndex = random.nextInt(results.length());
-                    JSONObject place = results.getJSONObject(randomIndex);
-                    meetingLocation = place.getString("name");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        thread.start();
-        try {
-            thread.join(); // Wait for the thread to finish
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return meetingLocation;
-    }
   
     private void createNewUser() {
         HandleDatabase handDb = HandleDatabase.getInstance();
